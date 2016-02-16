c9ddf1614e6fb4b95bdc6898bfa7bcb7cb04bf85
Haruka Ozaki <harukao.cb@gmail.com>
2016-02-16 15:01:49 +0900

    Create qacctTable.jl

---
+++ qacctTable.jl
@@ -0,0 +1,32 @@
+vars = Array{Any,1}()
+vals = Array{Any,1}()
+
+c = 0
+
+for line in eachline(STDIN)
+	if ismatch(r"======", line)
+		if length(vars) > 0
+			if c == 0
+				println(STDOUT, "| " * join(vars, " | ") * " |")
+				println(STDOUT, "|" * repeat("--|", length(vars)))
+				c += 1
+			end
+
+			println(STDOUT, "| " * join(vals, " | ") * " |")
+
+			empty!(vars)
+			empty!(vals)
+		end
+	elseif ismatch(r"hostname|jobnumber|jobname|ru_wallclock|maxvmem|taskid", chomp(line))
+		m = match(r"([^ ]+) +([^ ]+)", chomp(line)).captures
+		push!(vars, m[1])
+		push!(vals, m[2])
+	end
+end
+
+if length(vars) > 0
+	println(STDOUT, "| " * join(vals, " | ") * " |")
+
+	empty!(vars)
+	empty!(vals)
+end
